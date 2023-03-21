a801032ec0b4350e74ba2579b8f16ba47551e5d0
AhnYeonghoo <dksdudgn00@naver.com>
2023-03-22 08:36:15 +0900

    Create Repository & Root Templates

--- src/main/java/com/mysite/board/answer/Answer.java
+++ src/main/java/com/mysite/board/answer/Answer.java
@@ -1,32 +1,28 @@
-package com.mysite.board;
+package com.mysite.board.answer;
 
 
+import com.mysite.board.question.Question;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
-    @Column(length = 200)
-    private String subject;
 
     @Column(columnDefinition = "TEXT")
     private String content;
 
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+    @ManyToOne
+    private Question question;
 
 }
